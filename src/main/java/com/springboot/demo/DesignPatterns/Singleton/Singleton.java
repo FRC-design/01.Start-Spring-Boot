ce0a5dd5101db2ec49706631fcf55b155186b39b
frc <fengrucheng1@gmail.com>
2021-12-03 10:00:45 +0800

    big decimal learn

--- src/main/java/com/springboot/demo/DesignPatterns/Singleton/Singleton.java
+++ src/main/java/com/springboot/demo/DesignPatterns/Singleton/Singleton.java
@@ -1,95 +1,15 @@
 package com.springboot.demo.DesignPatterns.Singleton;
 
 public class Singleton {
-    public static class Singleton1{
-        private static Singleton1 instance = new Singleton1();
-
-
-        private Singleton1(){
-        }
-
-        public static Singleton1 getInstance(){
-            return instance;
-        }
-    }
-
-    public static class Singleton2{
-        private static Singleton2 instance;
-
-
-        private Singleton2(){
-        }
-
-        public static Singleton2 getInstance(){
-            if(instance == null){
-                instance = new Singleton2();
-            }
-            return instance;
-        }
-    }
-
-
-    public static class Singleton3{
-        private static Singleton3 instance;
-
-
-        private Singleton3(){
-        }
-
-        public static synchronized Singleton3 getInstance(){
-            if(instance == null){
-                instance  = new Singleton3();
-            }
-            return instance;
-        }
-    }
-
-    public static class Singleton4{
-
-        private static Singleton4 instance;
-        private Singleton4(){
-        }
-
-        public static  Singleton4 getInstance(){
-            if(instance == null){
-                synchronized (Singleton4.class){
-                    if(instance == null){
-                        instance  = new Singleton4();
-                    }
+    private volatile static Singleton instance = null;
+    public static Singleton getInstance() {
+        if(null == instance) {
+            synchronized (Singleton.class) {
+                if(null == instance) {
+                    instance = new Singleton();
                 }
-
             }
-            return instance;
         }
+        return instance;
     }
-
-    /**
-     * 懒汉模式
-     */
-
-    public static class SingletonFinal{
-
-        private volatile static  SingletonFinal instance = null;
-
-        private SingletonFinal(){
-        }
-
-        public static  SingletonFinal getInstance(){
-            if(instance == null){
-                synchronized (Singleton4.class){
-                    if(instance == null){
-                        instance  = new SingletonFinal();
-                    }
-                }
-
-            }
-            return instance;
-        }
-    }
-
-    /**
-     *1. 内部类，JVM保证只创建一个
-     *2. 枚举类，只创建一个
-     */
-
 }
